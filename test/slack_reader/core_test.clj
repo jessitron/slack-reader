2e117dc78d7d0d4a7a58a492de87890118ab9c79
Jessica Kerr <jessitron@outpace.com>
2015-02-20 04:20:30 +0000

    Valid channel names

--- test/slack_reader/core_test.clj
+++ test/slack_reader/core_test.clj
@@ -5,11 +5,33 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.string :as str]
+            [schema.core :as s]
             [schema.test]))
 
 (use-fixtures :once schema.test/validate-schemas)
 
-(def channel-gen (gen/no-shrink (gen/such-that #(not (str/blank? %)) gen/string)))
+(defn- to-and-back [to from f]
+                          (fn [& in]
+                            (from (apply f (map to in)))))
+(def char-range (to-and-back int (partial map char) range))
+;; lowercase letters, underscore, dash
+
+(def channel-name-allowed-chars (set (concat (char-range \a \z) (char-range \0 \9) [\_ \-])))
+(def ChannelNameChar (s/pred channel-name-allowed-chars "lowercase, digit, dash, underscore"))
+
+(def channel-char-gen (gen/elements channel-name-allowed-chars))
+(def disallowed? (partial contains? #{"_" "-"}))
+
+;; Can't be all underscores, or all dashes. Can be a combo of the two
+;; (by observation)
+(s/defn disallowed? [channel-chars :- [ChannelNameChar]]
+  (or (every? (partial = \_) channel-chars)
+      (every? (partial = \-) channel-chars)))
+
+(def channel-name-gen (gen/no-shrink
+                  (gen/such-that (complement disallowed?)
+                                 (gen/fmap (partial apply str)
+                                           (gen/vector channel-char-gen 1 21)))))
 
 (defspec test-api 10
   (prop/for-all [parameters (gen/map gen/keyword gen/string-alphanumeric)]
@@ -20,13 +42,15 @@
                        (is (= parameters parameter-echo)))
                   )))
 
+;; this could be in config with token
+(def my-username "jess")
 (deftest whoami
   (testing "can retrieve all users"
     (let [users-by-name (list-users)]
-      (is (not (nil? (get users-by-name "jess")))))))
+      (is (not (nil? (get users-by-name my-username)))))))
 
 ;; ha ha, need this for cleanup
-(def my-user {:id "U029S7MJ5"})
+(defn my-user [] (:id (get (list-users) my-username )))
 (defn archive-my-lonely-channels [user]
   (let [
         me (:id user)
@@ -41,21 +65,22 @@
   (let [response (call-slack "channels.list")]
     (is (ok response))
     (let [channel-ids (set (map :name (:channels response)))]
-      (is (channel-ids "cuteness"))
-      (is (channel-ids "pravda")))))
+      (is (channel-ids "general"))
+      (is (channel-ids "random")))))
 
 (deftest channel-existence
   (testing "channel that must exist"
-    (is (channel-exists? "cuteness")))
+    (is (channel-exists? "general")))
   (testing "channel that does not exist"
-    (is (not (channel-exists? "no-more-clojure"))))
+    (is (not (channel-exists? "do-not-create-this-channel"))))
   (testing "archived channels don't exist"
-    (is (not (channel-exists? "guest-support-rocks")))))
+    (is (not (channel-exists? "this-channel-is-archived")))))
 
 ;; TODO: work from archived channels instead of creating a million new ones
 ;; should be fair to un-archive, use it, re-archive
 (defspec create-and-delete-channel 2
-  (prop/for-all [name (gen/such-that #(not (channel-exists? %)) channel-gen)]
+  (prop/for-all [name (gen/such-that #(not (channel-exists? %)) channel-name-gen)]
+                (println (str "creating channel<" name) ">")
                 (let [channel-info (create-channel name)]
                   (and (is (channel-exists? name))
                        (do
